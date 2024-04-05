891c5379bf1354ab3e9cfd958a3404291908eb2e
BrunoSamuel2 <165202502+BrunoSamuel2@users.noreply.github.com>
2024-04-05 21:12:00 +0100

    agora tem de ir

--- funcoes.c
+++ funcoes.c
@@ -23,10 +23,10 @@ int lerMao(carta baralho[]) {
     //posicao no array da mao
     int pos;
 
-    wchar_t mao[16]={0};
+    wchar_t mao[28]={0};
 
     //funcao que coloca as cartas recebidas no array mao
-    if (fgetws(mao, 16, stdin)==NULL) {
+    if (fgetws(mao, 28, stdin)==NULL) {
         wprintf(L"O Scan da mão é inválido.\n");
         return 1;
     }
