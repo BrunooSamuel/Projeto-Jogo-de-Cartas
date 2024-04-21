8f583f37334123bcae7f749cbaea6e96c63898ad
BrunoSamuel2 <165202502+BrunoSamuel2@users.noreply.github.com>
2024-04-21 20:28:25 +0100

    trabalhando

--- comparacoes.c
+++ comparacoes.c
@@ -88,9 +88,9 @@ bool verificarJogada(carta baralho[], wchar_t *jogadaJogador, wchar_t *jogadasAn
     {   
         int i;
         int passosEncontrados = 0;
-        for(i = numAnte - 1; wcscmp(&jogadasAnte[i], L"PASSO\n") == 0; i--) passosEncontrados++;
-        wprintf(L"Numero de passos %d\n", passosEncontrados);
-        if(passosEncontrados == 3)
+        for(i = numAnte - 1; i > 0 && wcscmp(&jogadasAnte[i], L"PASSO\n") != 0; i--) passosEncontrados++;
+        //wprintf(L"Numero de passos %d\n", passosEncontrados);
+        if(passosEncontrados != 0)
         {
             verificarCombinacao(jogadaJogador, tamJogadaJogador, baralho, comb);
             return(comb[3] == 0);
@@ -121,7 +121,7 @@ bool verificarJogada(carta baralho[], wchar_t *jogadaJogador, wchar_t *jogadasAn
 bool verificarJogadacomReis(carta baralho[], wchar_t *jogadaJogador, wchar_t *jogadasAnte, int arrayTamanhos[], int tamJogadaJogador, int numAnte)
 
 {  
-    int comb[4] = {0};
+     int comb[4] = {0};
     if(numAnte == 0)
     {
 
