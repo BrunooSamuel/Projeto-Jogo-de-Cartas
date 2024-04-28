11575cd0b993f57f38bd822f1cf7f5a2d4975972
BrunoSamuel2 <165202502+BrunoSamuel2@users.noreply.github.com>
2024-04-28 12:55:56 +0100

    testes

--- gerar.c
+++ gerar.c
@@ -42,10 +42,48 @@ int analisarMao (carta baralho[], wchar_t mao[], int numero, int tamanho) {
 void gerarConjunto (carta baralho[], wchar_t mao[], int valorCartaMaisAlta,wchar_t codigoCartaMaisAlta, int tamAnterior, int tamMao) {
     wprintf(L"Veio para gerar Conjunto\n");
 
-    int numeroMaisAlta=numeroCarta(baralho, codigoCartaMaisAlta);  
-    int contagem=analisarMao (baralho, mao, numeroMaisAlta, tamMao);
-    wprintf(L"A mão tem %d cartas de número %d\n", contagem, numeroMaisAlta);
+    int numeroMaisAlta=numeroCarta(baralho, codigoCartaMaisAlta);
+    for (int i = numeroMaisAlta; i <= 14; i++)
+    {
+        int contagem=analisarMao (baralho, mao, i, tamMao);
+        // wprintf(L"A mão tem %d cartas de número %d\n", contagem, i);
 
+        if (contagem>=tamAnterior) 
+        {
+            //wprintf(L"Pode haver conjunto no numero %d, pq tem %d cartas\n", i, contagem);
+            imprimirConjuntos (baralho, mao, i, contagem, tamAnterior, tamMao);
+        }
+    }
+
+
+}
+
+void imprimirConjuntos (carta baralho[], wchar_t mao[], int numero, int contagem, int tamAnterior, int tamMao) {
+    bool encontrou=false;
+    int tamanho=1;
+
+    for (int i = 0; i < tamMao && tamanho<=tamAnterior; i++)
+    {
+        for (int k = (numero*4)-4; k <= (numero*4)-1 && !encontrou; k++)
+        {
+            if (mao[i]==baralho[k].codigo) 
+            {
+                if (tamAnterior==tamanho) 
+                {
+                    wprintf("%lc\n", mao[i]);
+                    encontrou=true;
+                    tamanho++;
+                }
+                else 
+                {
+                    wprintf("%lc ", mao[i]);
+                    encontrou=true;
+                    tamanho++;
+                }
+            }
+        }
+        encontrou=false;
+    }
 }
 
 void gerarSequencia (carta baralho[], wchar_t mao[], int valorCartaMaisAlta,wchar_t codigoCartaMaisAlta, int tamAnterior, int tamMao) {
@@ -53,6 +91,9 @@ void gerarSequencia (carta baralho[], wchar_t mao[], int valorCartaMaisAlta,wcha
 
     int numeroMaisAlta=numeroCarta(baralho, codigoCartaMaisAlta);  
     int contagem=analisarMao (baralho, mao, numeroMaisAlta, tamMao);
+
+
+
     wprintf(L"A mão tem %d cartas de número %d\n", contagem, numeroMaisAlta);
 }
 
