12c3be8a1810d7e4b57aab64d58b2a63bfc3e021
jota <102486574+jotadt@users.noreply.github.com>
2024-04-29 15:11:49 +0100

    gerarSequencia
    Problema no print das sequencias e na identificação da maior sequencia

--- gerar.c
+++ gerar.c
@@ -47,14 +47,62 @@ void gerarConjunto (carta baralho[], wchar_t mao[], int valorCartaMaisAlta,wchar
     wprintf(L"A mão tem %d cartas de número %d\n", contagem, numeroMaisAlta);
 
 }
-
-void gerarSequencia (carta baralho[], wchar_t mao[], int valorCartaMaisAlta,wchar_t codigoCartaMaisAlta, int tamAnterior, int tamMao) {
-    wprintf(L"Veio para gerar Sequencia\n");
-
-    int numeroMaisAlta=numeroCarta(baralho, codigoCartaMaisAlta);  
-    int contagem=analisarMao (baralho, mao, numeroMaisAlta, tamMao);
-    wprintf(L"A mão tem %d cartas de número %d\n", contagem, numeroMaisAlta);
+// Criei essa função para usar como teste na gerarSequencia
+void imprimirSequencia(wchar_t* sequencia, int tamanho) {
+    wprintf(L"Sequência maior encontrada: ");
+    for (int i = 0; i < tamanho; i++) {
+        wprintf(L"%lc ", sequencia[i]);
+    }
+    wprintf(L"\n");
 }
+void gerarSequencia (carta baralho[], wchar_t mao[], int valorCartaMaisAlta, wchar_t codigoCartaMaisAlta, int tamAnterior, int tamMao) {
+    wprintf(L"Veio para gerar Sequencia\n"); 
+
+    int numeroMaisAlta = numeroCarta(baralho, codigoCartaMaisAlta);  
+    int contagem = analisarMao(baralho, mao, numeroMaisAlta, tamMao);
+    wprintf(L"A mão tem %d cartas de número %d\n", contagem, numeroMaisAlta);
+
+    wchar_t* sequenciaSuperior = NULL;
+    int sequenciaAtual = 0;
+    int sequenciaMaior = 0;
+
+    for (int i = 0; i < tamMao - 1; i++) {
+        wchar_t cartaAtual = mao[i];
+        wchar_t proximaCarta = mao[i + 1];
+        int numeroCartaAtual = numeroCarta(baralho, cartaAtual);
+        int numeroProximaCarta = numeroCarta(baralho, proximaCarta);
+
+        if (numeroProximaCarta == numeroCartaAtual + 1) {
+            sequenciaAtual++;
+
+            if (sequenciaAtual > sequenciaMaior) {
+                sequenciaMaior = sequenciaAtual;
+
+                free(sequenciaSuperior);
+
+                sequenciaSuperior = (wchar_t*)malloc(sequenciaMaior * sizeof(wchar_t));
+
+              
+                for (int j = 0; j < sequenciaMaior; j++) {
+                    sequenciaSuperior[j] = mao[i - sequenciaMaior + j + 1];
+                }
+            }
+        } else {
+            sequenciaAtual = 0;
+        }
+    }
+    
+    if (sequenciaMaior >= tamAnterior) {
+        wprintf(L"Encontrada uma sequência superior na mão.\n");
+        imprimirSequencia(sequenciaSuperior, sequenciaMaior);
+    } else {
+        wprintf(L"Não foi encontrada uma sequência superior na mão.\n");
+    }
+
+    free(sequenciaSuperior);
+}
+
+
 
 void gerarDuplaSequencia (carta baralho[], wchar_t mao[], int valorCartaMaisAlta,wchar_t codigoCartaMaisAlta, int tamAnterior, int tamMao) {
     wprintf(L"Veio para gerar Dupla Sequencia\n");
